9291bbd465b3453d1e95d3d995d310fa8b449343
Mitchxxx <megboko@gmail.com>
2024-02-05 13:48:42 +0000

    Updated ami

--- Jenkins-Server-TF/data.tf
+++ Jenkins-Server-TF/data.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "example" {
+data "aws_ami" "ubuntu" {
   most_recent = true
   owners = ["099720109477"]
   filter {
     name   = "name"
-    values = ["amazon/ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
--- Jenkins-Server-TF/main.tf
+++ Jenkins-Server-TF/main.tf
@@ -90,6 +90,7 @@ module "ec2_instance" {
 
   instance_type               = var.instance_type
   key_name                    = "terraform-key"
+  ami     = data.aws_ami.ubuntu.id
   monitoring                  = true
   vpc_security_group_ids      = [module.sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
